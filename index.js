8488202461c5794ccdd1b019c91f148b3e61db5b
Leandro1308 <remunerando@hotmail.com>
2025-04-03 09:22:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const path = require("path");
 
-// Caminho correto e absoluto para rodar tanto local quanto no Railway
-require(path.join(__dirname, "marketing-multinivel", "index.js"));
+// Garante que o caminho funcione tanto local quanto no Railway
+require(path.resolve(__dirname, "marketing-multinivel", "index.js"));
