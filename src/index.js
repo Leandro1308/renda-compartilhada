cfd435c29c94802c2ee9d31a41ce4ba21d1d0cae
Leandro1308 <remunerando@hotmail.com>
2025-04-10 20:25:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+  .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 const express = require('express')
 const app = express()
 
