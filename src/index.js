7f0360f445ffa182f4a17093d4c2c7221966893c
Leandro1308 <remunerando@hotmail.com>
2025-04-10 20:26:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
+
+// Conexão com o MongoDB usando variável de ambiente do Railway
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Conectado ao MongoDB com sucesso!'))
   .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
-const express = require('express')
-const app = express()
 
+// Rota principal (apenas pra teste se API está online)
 app.get('/', (req, res) => {
-  res.send('API está funcionando perfeitamente!')
-})
+  res.send('API está funcionando perfeitamente!');
+});
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
