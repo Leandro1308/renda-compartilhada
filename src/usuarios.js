6f7b136658818c889b219395f1f0a8fb91db882c
Leandro1308 <remunerando@hotmail.com>
2025-04-11 11:30:44 -0300

    Update usuarios.js

--- src/usuarios.js
+++ src/usuarios.js
@@ -1,38 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const path = './src/usuarios.json';
+const path = require('path');
+
+const filePath = path.join(__dirname, 'usuarios.json');
+
+const readData = () => JSON.parse(fs.readFileSync(filePath));
 
 router.get('/', (req, res) => {
-  const usuarios = JSON.parse(fs.readFileSync(path));
-  res.json(usuarios);
+  res.json(readData());
 });
 
-router.post('/cadastro', (req, res) => {
-  const usuarios = JSON.parse(fs.readFileSync(path));
-  const novo = req.body;
-
-  novo.id = Date.now();
-  novo.linkAfiliado = `https://clubedamente.com/inscricao/${novo.id}`;
-  novo.contaBancaria = "";
-  novo.indicados = [];
-  novo.indicador = req.body.indicador || null;
-
-  usuarios.push(novo);
-  fs.writeFileSync(path, JSON.stringify(usuarios, null, 2));
-  res.json({ mensagem: 'Usuário cadastrado com sucesso!', usuario: novo });
-});
-
-router.put('/conta/:id', (req, res) => {
-  const usuarios = JSON.parse(fs.readFileSync(path));
-  const { id } = req.params;
-  const usuario = usuarios.find(u => u.id == id);
-
-  if (!usuario) return res.status(404).json({ erro: 'Usuário não encontrado' });
-
-  usuario.contaBancaria = req.body.contaBancaria;
-  fs.writeFileSync(path, JSON.stringify(usuarios, null, 2));
-  res.json({ mensagem: 'Conta bancária cadastrada com sucesso!' });
+router.post('/', (req, res) => {
+  const usuarios = readData();
+  usuarios.push(req.body);
+  fs.writeFileSync(filePath, JSON.stringify(usuarios, null, 2));
+  res.status(201).send('Usuário criado com sucesso!');
 });
 
 module.exports = router;
