7a7b60db49ee976fa6fc26060596ae31a9c407f3
Leandro1308 <remunerando@hotmail.com>
2025-04-11 12:09:05 -0300

    Update pagamentos.js

--- src/pagamentos.js
+++ src/pagamentos.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, 'pagamentos.json');
+const arquivoPagamentos = path.join(__dirname, 'pagamentos.json');
 
-const readData = () => JSON.parse(fs.readFileSync(filePath));
-
+// Buscar todos os pagamentos
 router.get('/', (req, res) => {
-  res.json(readData());
+  const pagamentos = JSON.parse(fs.readFileSync(arquivoPagamentos));
+  res.json(pagamentos);
 });
 
+// Criar novo pagamento
 router.post('/', (req, res) => {
-  const pagamentos = readData();
-  pagamentos.push(req.body);
-  fs.writeFileSync(filePath, JSON.stringify(pagamentos, null, 2));
-  res.status(201).send('Pagamento registrado com sucesso!');
+  const pagamentos = JSON.parse(fs.readFileSync(arquivoPagamentos));
+  const novoPagamento = req.body;
+  novoPagamento.id = Date.now();
+  pagamentos.push(novoPagamento);
+  fs.writeFileSync(arquivoPagamentos, JSON.stringify(pagamentos, null, 2));
+  res.status(201).json(novoPagamento);
 });
 
 module.exports = router;
