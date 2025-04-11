6ee3a243bc5048757027534457aade7fd6cef36e
Leandro1308 <remunerando@hotmail.com>
2025-04-11 10:27:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Estrutura inicial da API do Clube da Mente
-
 const express = require('express');
 const app = express();
+const path = require('path');
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas principais
 const usuarios = require('./src/usuarios');
