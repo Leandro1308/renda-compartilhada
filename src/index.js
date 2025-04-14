8b848e799d35f2c1615657ea0ecdcb6db8b4f0ec
Leandro1308 <remunerando@hotmail.com>
2025-04-14 14:52:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 require('dotenv').config();
+const connectDB = require('./db');
+
+const app = express();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// Rotas principais
 const usuarios = require('./usuarios');
 const saques = require('./saques');
 const pagamentos = require('./pagamentos');
