64283c966b86e24d5f2fac1401c2f1581b51992d
Leandro1308 <remunerando@hotmail.com>
2025-04-14 14:51:49 -0300

    Create auth.js

---
+++ src/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ erro: 'Token não fornecido' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuarioId = decoded.id;
+    next();
+  } catch (error) {
+    res.status(401).json({ erro: 'Token inválido' });
+  }
+};
